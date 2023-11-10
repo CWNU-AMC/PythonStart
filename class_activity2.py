8acbf29c01abdd2789106b120c74e3f7d767813e
Aziz Matin <azizmatteen@gmail.com>
2023-11-10 23:08:42 +0800

    Second class activity

---
+++ class_activity2.py
@@ -0,0 +1,13 @@
+req_num = input("Enter the number")
+
+def reversnum(no):
+    var = str(no)
+
+    b = var[::-1]
+    if var == b:
+        return "True"
+    else:
+        return "False"
+
+
+print(reversnum(req_num))
